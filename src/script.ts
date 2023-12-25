8c43e5737081dbcc615498b7ff633f7a8b6a6b8c
saltbread1 <takenokonosato913@icloud.com>
2023-12-25 19:23:13 +0900

    display a polygon

--- src/script.ts
+++ src/script.ts
@@ -1,15 +1,15 @@
-const initCanvas = () : void => {
-    const c : HTMLCanvasElement = document.getElementById("canvas") as HTMLCanvasElement;
+const initCanvas = (): void => {
+    const c: HTMLCanvasElement = document.getElementById("canvas") as HTMLCanvasElement;
     c.width = 512;
     c.height = 512;
 
-    const gl : WebGLRenderingContext = c.getContext("webgl");
+    const gl: WebGLRenderingContext = c.getContext("webgl") as WebGLRenderingContext;
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 };
 
-const setShader = () : void => {
-    const vs : string = `
+const setShader = (): void => {
+    const vs: string = `
         attribute vec3 position;
         
         void main() {
@@ -17,40 +17,41 @@ const setShader = () : void => {
         }
     `;
 
-    const fs : string = `
+    const fs: string = `
         void main() {
             gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
         }
     `;
 
-    const c : HTMLCanvasElement = document.getElementById("canvas") as HTMLCanvasElement;
-    const gl : WebGLRenderingContext = c.getContext("webgl");
+    const c: HTMLCanvasElement = document.getElementById("canvas") as HTMLCanvasElement;
+    const gl: WebGLRenderingContext = c.getContext("webgl") as WebGLRenderingContext;
 
-    const vShader : WebGLShader = createShader(gl, gl.VERTEX_SHADER, vs);
-    const fShader : WebGLShader = createShader(gl, gl.FRAGMENT_SHADER, fs);
+    const vShader: WebGLShader = createShader(gl, gl.VERTEX_SHADER, vs) as WebGLShader;
+    const fShader: WebGLShader = createShader(gl, gl.FRAGMENT_SHADER, fs) as WebGLShader;
 
-    const program : WebGLProgram = createProgram(gl, vShader, fShader);
+    const program: WebGLProgram = createProgram(gl, vShader, fShader) as WebGLProgram;
 
-    const attLocation : number = gl.getAttribLocation(program, "position");
-    const attStride : number = 3;
+    const attLocation: number = gl.getAttribLocation(program, "position");
+    const attStride: number = 3;
 
-    const vertexPosition : number[] = [
+    const vertexPosition: number[] = [
         0.0, 1.0, 0.0,
         1.0, 0.0, 0.0,
         -1.0, 0.0, 0.0,
     ];
 
-    const vbo : WebGLBuffer = createVBO(gl, vertexPosition);
-    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+    const vbo: WebGLBuffer = createVBO(gl, vertexPosition);
     gl.enableVertexAttribArray(attLocation);
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
     gl.vertexAttribPointer(attLocation, attStride, gl.FLOAT, false, 0, 0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
     gl.drawArrays(gl.TRIANGLES, 0, 3);
     gl.flush();
 };
 
-const createShader = (gl: WebGLRenderingContext, type: number, s: string) : WebGLShader => {
-    const shader: WebGLShader = gl.createShader(type);
+const createShader = (gl: WebGLRenderingContext, type: number, s: string): WebGLShader | null => {
+    const shader: WebGLShader = gl.createShader(type) as WebGLShader;
     gl.shaderSource(shader, s);
     gl.compileShader(shader);
 
@@ -59,11 +60,12 @@ const createShader = (gl: WebGLRenderingContext, type: number, s: string) : WebG
     }
     else {
         alert(gl.getShaderInfoLog(shader));
+        return null;
     }
 };
 
-const createProgram = (gl: WebGLRenderingContext, vs: WebGLShader, fs: WebGLShader) : WebGLProgram => {
-    const program : WebGLProgram = gl.createProgram();
+const createProgram = (gl: WebGLRenderingContext, vs: WebGLShader, fs: WebGLShader): WebGLProgram | null => {
+    const program: WebGLProgram = gl.createProgram() as WebGLProgram;
     gl.attachShader(program, vs);
     gl.attachShader(program, fs);
     gl.linkProgram(program);
@@ -74,11 +76,12 @@ const createProgram = (gl: WebGLRenderingContext, vs: WebGLShader, fs: WebGLShad
     }
     else {
         alert(gl.getProgramInfoLog(program));
+        return null;
     }
 };
 
-const createVBO = (gl: WebGLRenderingContext, data: number[]) : WebGLBuffer => {
-    const vbo : WebGLBuffer = gl.createBuffer();
+const createVBO = (gl: WebGLRenderingContext, data: number[]): WebGLBuffer => {
+    const vbo: WebGLBuffer = gl.createBuffer() as WebGLBuffer;
     gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
