14ecc0cbbf8b452f713e9375284e3aa0edf36974
saltbread1 <takenokonosato913@icloud.com>
2023-12-25 20:54:40 +0900

    animation

--- src/script.ts
+++ src/script.ts
@@ -11,6 +11,9 @@ const initCanvas = (): void => {
 };
 
 const setShader = (): void => {
+    const fps: number = 30;
+    const initTime: number = new Date().getTime();
+
     const vs: string = readFileSync('./src/shader/vertex.glsl', 'utf-8');
     const fs: string = readFileSync('./src/shader/fragment.glsl', 'utf-8');
 
@@ -25,7 +28,9 @@ const setShader = (): void => {
     const attLocation: number = gl.getAttribLocation(program, 'position');
     const attStride: number = 3;
 
-    const uniLocation: WebGLUniformLocation = gl.getUniformLocation(program, 'resolution') as WebGLUniformLocation;
+    const uniLocations: WebGLUniformLocation[] = [];
+    uniLocations.push(gl.getUniformLocation(program, 'resolution') as WebGLUniformLocation);
+    uniLocations.push(gl.getUniformLocation(program, 'time') as WebGLUniformLocation);
 
     const vertexPosition: number[] = [
         -0.5,  0.5,  0.0,
@@ -42,17 +47,26 @@ const setShader = (): void => {
     const vbo: WebGLBuffer = createVBO(gl, vertexPosition);
     const ibo: WebGLBuffer = createIBO(gl, index);
 
-    gl.enableVertexAttribArray(attLocation);
-    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-    gl.vertexAttribPointer(attLocation, attStride, gl.FLOAT, false, 0, 0);
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    setAttribute(gl, vbo, attLocation, attStride);
 
-    gl.uniform2f(uniLocation, c.width, c.height);
+    gl.uniform2f(uniLocations[0], c.width * 0.5, c.height * 0.5);
 
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
-    gl.drawElements(gl.TRIANGLES, index.length, gl.UNSIGNED_SHORT, 0);
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-    gl.flush();
+    const render = (): void => {
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        const time: number = (new Date().getTime() - initTime) * 0.001;
+        gl.uniform1f(uniLocations[1], time);
+
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
+        gl.drawElements(gl.TRIANGLES, index.length, gl.UNSIGNED_SHORT, 0);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+        gl.flush();
+
+        setTimeout(render, 1000 / fps);
+    };
+
+    render();
 };
 
 const createShader = (gl: WebGLRenderingContext, type: number, s: string): WebGLShader | null => {
@@ -99,6 +113,13 @@ const createIBO = (gl: WebGLRenderingContext, data: number[]): WebGLBuffer => {
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
 
     return ibo;
+};
+
+const setAttribute = (gl: WebGLRenderingContext, vbo: WebGLBuffer, attLocation: number, attStride: number): void => {
+    gl.enableVertexAttribArray(attLocation);
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+    gl.vertexAttribPointer(attLocation, attStride, gl.FLOAT, false, 0, 0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
 };
 
 window.addEventListener('DOMContentLoaded', initCanvas);
--- src/shader/fragment.glsl
+++ src/shader/fragment.glsl
@@ -2,10 +2,14 @@
 precision mediump float;
 #endif
 
+const float PI = acos(-1.);
+
 uniform vec2 resolution;
+uniform float time;
 
 void main() {
-    vec2 p = (gl_FragCoord.xy * 2.0 - resolution) / min(resolution.x, resolution.y);
-    vec3 col = (p.xyx + 1.0) * 0.5;
+    vec2 uv = (gl_FragCoord.xy * 2.0 - resolution) / min(resolution.x, resolution.y);
+    uv = (uv + vec2(cos(time * PI), sin(time * PI)) + 2.0) * 0.25;
+    vec3 col = uv.xyy;
     gl_FragColor = vec4(col, 1.0);
 }
