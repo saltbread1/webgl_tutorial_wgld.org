5fe053902e309ebcc9358e728b9152b0eb70a83a
saltbread1 <takenokonosato913@icloud.com>
2024-01-23 02:22:05 +0900

    phong shading

--- src/shader/fragment.glsl
+++ src/shader/fragment.glsl
@@ -3,15 +3,21 @@ precision mediump float;
 #endif
 
 const float PI = acos(-1.);
-
 uniform vec2 resolution;
 uniform float time;
+uniform mat4 invMatrix;
+uniform vec3 lightDirection;
+uniform vec3 eyeDirection;
+uniform vec4 ambientColor;
+varying vec3 vNormal;
 varying vec4 vColor;
 
 void main() {
-//    vec2 uv = (gl_FragCoord.xy * 2.0 - resolution) / min(resolution.x, resolution.y);
-//    uv = (uv + vec2(cos(time * PI), sin(time * PI)) + 2.0) * 0.25;
-//    vec3 col = uv.xyy;
-//    gl_FragColor = vec4(col, 1.0);
-    gl_FragColor = vColor;
+    vec3 invLight = normalize(invMatrix * vec4(lightDirection, 0.0)).xyz;
+    vec3 invEye = normalize(invMatrix * vec4(eyeDirection, 0.0)).xyz;
+    float diffse = clamp(dot(vNormal, invLight), 0.0, 1.0);
+    vec3 reflectDirection = reflect(-invLight, vNormal);
+    float specular = pow(clamp(dot(reflectDirection, invEye), 0.0, 1.0), 16.0);
+    vec4 light = vColor * vec4(vec3(diffse + specular), 1.0);
+    gl_FragColor = light + ambientColor;
 }
--- src/shader/vertex.glsl
+++ src/shader/vertex.glsl
@@ -6,19 +6,11 @@ attribute vec3 position;
 attribute vec3 normal;
 attribute vec4 color;
 uniform mat4 mvpMatrix;
-uniform mat4 invMatrix;
-uniform vec3 lightDirection;
-uniform vec3 eyeDirection;
-uniform vec4 ambientColor;
+varying vec3 vNormal;
 varying vec4 vColor;
 
 void main() {
-    vec3 invLight = normalize(invMatrix * vec4(lightDirection, 0.0)).xyz;
-    vec3 invEye = normalize(invMatrix * vec4(eyeDirection, 0.0)).xyz;
-    float diffse = clamp(dot(normal, invLight), 0.0, 1.0);
-    vec3 reflectDirection = reflect(-invLight, normal);
-    float specular = pow(clamp(dot(reflectDirection, invEye), 0.0, 1.0), 16.0);
-    vec4 light = color * vec4(vec3(diffse + specular), 1.0);
-    vColor = light + ambientColor;
+    vNormal = normal;
+    vColor = color;
     gl_Position = mvpMatrix * vec4(position, 1.0);
 }
