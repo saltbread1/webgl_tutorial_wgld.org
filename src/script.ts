1d6e6f588bcd7b95d08734d2ab6654e2ff5f33b3
saltbread1 <takenokonosato913@icloud.com>
2024-01-22 21:20:57 +0900

    torus

--- src/script.ts
+++ src/script.ts
@@ -6,6 +6,12 @@ type Attribute = {
     attStride: number;
 };
 
+type Vertices = {
+    pos: number[];
+    col: number[];
+    idx: number[];
+};
+
 const initCanvas = (): void => {
     const c: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement;
     c.width = 512;
@@ -20,8 +26,8 @@ const initCanvas = (): void => {
     // gl.enable(gl.CULL_FACE);
 
     // enable depth test
-    // gl.enable(gl.DEPTH_TEST);
-    // gl.depthFunc(gl.LEQUAL);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
 };
 
 const setShader = (): void => {
@@ -50,29 +56,12 @@ const setShader = (): void => {
 
     gl.uniform2f(uniLocations.get('resolution') as WebGLUniformLocation, c.width, c.height);
 
-    const vertexPosition: number[] = [
-        -1.0,  1.0,  0.0,
-        -1.0, -1.0,  0.0,
-         1.0, -1.0,  0.0,
-         1.0,  1.0,  0.0,
-    ];
-
-    const vertexColor: number[] = [
-        1.0, 0.0, 0.0, 1.0,
-        0.0, 1.0, 0.0, 1.0,
-        0.0, 0.0, 1.0, 1.0,
-        1.0, 1.0, 1.0, 1.0,
-    ];
-
-    const index: number[] = [
-        0, 1, 2, // ccw
-        0, 3, 2, // cw
-    ];
+    const vertices: Vertices = torus(100, 100, 0.2, 1);
 
     const vboMap: Map<string, WebGLBuffer> = new Map<string, WebGLBuffer>();
-    vboMap.set('position', createVBO(gl, vertexPosition));
-    vboMap.set('color', createVBO(gl, vertexColor));
-    const ibo: WebGLBuffer = createIBO(gl, index);
+    vboMap.set('position', createVBO(gl, vertices.pos));
+    vboMap.set('color', createVBO(gl, vertices.col));
+    const ibo: WebGLBuffer = createIBO(gl, vertices.idx);
 
     setAttribute(gl, vboMap.get('position') as WebGLBuffer, attributes.get('position') as Attribute);
     setAttribute(gl, vboMap.get('color') as WebGLBuffer, attributes.get('color') as Attribute);
@@ -85,7 +74,7 @@ const setShader = (): void => {
     const mvpMatrix: glMat.mat4 = glMat.mat4.create();
 
     // calculate view x projection matrix
-    glMat.mat4.lookAt(vMatrix, [0.0, 0.0, 3.0], [0.0, 0.0, 0.0], [0.0, 1.0, 0.0]);
+    glMat.mat4.lookAt(vMatrix, [0.0, 1.0, 3.0], [0.0, 0.0, 0.0], [0.0, 1.0, 0.0]);
     glMat.mat4.perspective(pMatrix, 90, c.width / c.height, 0.1, 100);
     glMat.mat4.multiply(tmpMatrix, vMatrix, tmpMatrix);
     glMat.mat4.multiply(tmpMatrix, pMatrix, tmpMatrix);
@@ -107,7 +96,7 @@ const setShader = (): void => {
         glMat.mat4.multiply(mvpMatrix, tmpMatrix, mMatrix);
         gl.uniformMatrix4fv(uniLocations.get('mvpMatrix') as WebGLUniformLocation, false, mvpMatrix);
         // draw the model to the buffer
-        gl.drawElements(gl.TRIANGLES, index.length, gl.UNSIGNED_SHORT, 0);
+        gl.drawElements(gl.TRIANGLES, vertices.idx.length, gl.UNSIGNED_SHORT, 0);
 
         // model 2: rotate on y axis
         // MVP matrix
@@ -116,7 +105,7 @@ const setShader = (): void => {
         glMat.mat4.multiply(mvpMatrix, tmpMatrix, mMatrix);
         gl.uniformMatrix4fv(uniLocations.get('mvpMatrix') as WebGLUniformLocation, false, mvpMatrix);
         // draw the model to the buffer
-        gl.drawElements(gl.TRIANGLES, index.length, gl.UNSIGNED_SHORT, 0);
+        gl.drawElements(gl.TRIANGLES, vertices.idx.length, gl.UNSIGNED_SHORT, 0);
 
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
         gl.flush();
@@ -178,6 +167,67 @@ const setAttribute = (gl: WebGLRenderingContext, vbo: WebGLBuffer, attribute: At
     gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
     gl.vertexAttribPointer(attribute.attLocation, attribute.attStride, gl.FLOAT, false, 0, 0);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
+};
+
+const torus = (lRes: number, mRes: number, sRad: number, lRad: number): Vertices => {
+    const vertices: Vertices = {pos: [], col: [], idx: []};
+    for (let lon: number = 0; lon <= lRes; lon++) {
+        const t: number = Math.PI * 2 / lRes * lon;
+        for (let mer: number = 0; mer <= mRes; mer++) {
+            // position
+            const p: number = Math.PI * 2 / mRes * mer;
+            const x: number = lRad * Math.cos(t) + sRad * Math.cos(p) * Math.cos(t);
+            const y: number = lRad * Math.sin(t) + sRad * Math.cos(p) * Math.sin(t);
+            const z: number = sRad * Math.sin(p);
+            vertices.pos.push(x, y, z);
+
+            // color
+            const rgb: number[] = hsv2rgb(lon/lRes, 1, 1, 1);
+            vertices.col.push(rgb[0], rgb[1], rgb[2], rgb[3]);
+        }
+    }
+
+    for (let lon: number = 0; lon < lRes; lon++) {
+        for (let mer: number = 0; mer < mRes; mer++) {
+            const i: number = mer + (mRes+1) * lon;
+            vertices.idx.push(i, i+1, i+mRes+1);
+            vertices.idx.push(i+1, i+mRes+2, i+mRes+1);
+        }
+    }
+
+    return vertices;
+};
+
+const hsv2rgb = (h: number, s: number, v: number, a: number): number[] => {
+    const r: number = ((clamp01(Math.abs(fract(h+0/3)*6-3)-1)-1)*s+1)*v;
+    const g: number = ((clamp01(Math.abs(fract(h+2/3)*6-3)-1)-1)*s+1)*v;
+    const b: number = ((clamp01(Math.abs(fract(h+1/3)*6-3)-1)-1)*s+1)*v;
+    return [r, g, b, a];
+};
+
+const rgb2hsb = (r: number, g: number, b: number, a: number): number[] => {
+    const max: number = Math.max(Math.max(r, g), b);
+    const min: number = Math.min(Math.min(r, g), b);
+    const v: number = max;
+    const s: number = (max-min)/max;
+    let h: number = step(b, min)*((g-r)/(max-min)+1.)
+    + step(r, min)*((b-g)/(max-min)+3.)
+    + step(g, min)*((r-b)/(max-min)+5.);
+    h /= 6;
+    h += step(h, 0);
+    return [h, s, v, a];
+};
+
+const fract = (x: number): number => {
+    return x - Math.floor(x);
+};
+
+const clamp01 = (x: number): number => {
+    return Math.max(Math.min(x, 1), 0);
+};
+
+const step = (a: number, x: number): number => {
+    return x < a ? 0 : 1;
 };
 
 window.addEventListener('DOMContentLoaded', initCanvas);
